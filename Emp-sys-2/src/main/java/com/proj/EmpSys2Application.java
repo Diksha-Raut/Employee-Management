686a67b87aa462434d4ebd813b9346a125e207e0
Diksha Raut <54407758+Diksha-Raut@users.noreply.github.com>
2024-01-19 19:57:42 +0530

    Add files via upload

---
+++ Emp-sys-2/src/main/java/com/proj/EmpSys2Application.java
@@ -0,0 +1,13 @@
+package com.proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpSys2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpSys2Application.class, args);
+	}
+
+}
---
+++ Emp-sys-2/src/main/java/com/proj/service/EmpService.java
@@ -0,0 +1,36 @@
+package com.proj.service;
+
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.proj.entity.Employee;
+import com.proj.repo.EmpRepo;
+@Service
+public class EmpService {
+	@Autowired
+	private EmpRepo repo;
+	
+	public void addEmp(Employee e) {
+	repo.save(e);	}
+	
+	public List<Employee> getAllEmp() {
+		return repo.findAll();
+	}
+
+	public Employee getEmpById(int id) {
+		Optional<Employee> e = repo.findById(id);
+		if (e.isPresent()) {
+			return e.get();
+		}
+		return null;
+	}
+
+	public void deleteEmp(int id) {
+	repo.deleteById(id);
+	}
+}
